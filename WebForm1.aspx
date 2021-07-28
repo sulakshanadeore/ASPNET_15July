40d35bf4c1096f3abe5ca8f8b2e6853625a3c03c
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-07-28 12:11:17 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ServerSideStateMgtDemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/About.aspx">AboutUs</asp:HyperLink>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Show Value in the Session" />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerSideStateMgtDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Session ID=  " + Session.SessionID);
+            Response.Write("<br/>");
+            Response.Write("Is a New Session= " + Session.IsNewSession);
+            Session["mydata"] = "Hello";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write(Session["mydata"].ToString());
+        }
+    }
+}
