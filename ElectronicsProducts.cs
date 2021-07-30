c8aa7a103c76c9958870b3616b183ee9d79f802e
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-07-30 15:10:12 +0530

    Add files via upload

---
+++ ElectronicsProducts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMVCApp.Models
+{
+    public class ElectronicsProducts
+    {
+        public int ProdId { get; set; }
+        public string ProdName { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMVCApp.Models;
+namespace FirstMVCApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        static List<ElectronicsProducts> prodList = new List<ElectronicsProducts>();
+        //IEnumerable
+        public ActionResult Index()
+        {
+
+            return View(prodList);
+        }
+        
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public RedirectToRouteResult AddProduct(ElectronicsProducts data)
+        {
+            //ModelBinder
+            ElectronicsProducts p = new ElectronicsProducts();
+            p.ProdId = data.ProdId;
+            p.ProdName = data.ProdName;
+            p.Price = data.Price;
+            prodList.Add(p);
+            //return Content("Product successfully added to the list");
+            return RedirectToAction("Index");
+        }
+
+    }
+}
