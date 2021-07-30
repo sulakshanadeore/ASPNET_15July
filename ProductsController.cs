1a32509ed18188aeb388d96b4fcf292d8718487f
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-07-30 17:14:40 +0530

    Add files via upload

---
+++ ProductsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMVCApp.Models;
+namespace FirstMVCApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        static List<ElectronicsProducts> prodList = new List<ElectronicsProducts>();
+        //IEnumerable
+        public ActionResult Index()
+        {
+
+            return View(prodList);
+        }
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public RedirectToRouteResult AddProduct(ElectronicsProducts data)
+        {
+            //ModelBinder
+            ElectronicsProducts p = new ElectronicsProducts();
+            p.ProdId = data.ProdId;
+            p.ProdName = data.ProdName;
+            p.Price = data.Price;
+            prodList.Add(p);
+            //return Content("Product successfully added to the list");
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveProduct(int id)
+        {
+            ElectronicsProducts product=prodList.Find(prod => prod.ProdId == id);
+            prodList.Remove(product);
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult FindProduct(int id)
+        {
+            ElectronicsProducts product = prodList.Find(prod => prod.ProdId == id);
+
+            return View(product);
+        }
+
+        public ActionResult UpdateProduct(int id)
+        {
+            ElectronicsProducts product = prodList.Find(prod => prod.ProdId == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProduct(int id,ElectronicsProducts data)
+        {
+            ElectronicsProducts product = prodList.Find(prod => prod.ProdId == id);
+            product.ProdName = data.ProdName;
+            product.Price = data.Price;
+
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
