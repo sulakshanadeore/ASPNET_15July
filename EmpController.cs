4f0d746c7899f5b319b3ec6b660b04ed43197b52
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-08-04 14:50:38 +0530

    Add files via upload

---
+++ EmpController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    [EnableCors(origins:"*",headers:"*",methods:"*")]
+    public class EmpController : ApiController
+    {
+        static List<Empl> emps = new List<Empl>();
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IEnumerable<Empl> GetEmployees()
+        {
+            emps.Add(new Empl {Empid=1,Ename="Ajit",Sal=10000 });
+            emps.Add(new Empl { Empid = 2, Ename = "Sujit", Sal = 20000 });
+
+            return emps;
+        }
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public Empl GetEmployeeById(int id)
+        {
+            Empl empfound=emps.Find(e => e.Empid == id);
+            return empfound;
+        }
+
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public void PostEmployee([FromBody]Empl emp)
+        {
+            emps.Add(emp);
+            
+        }
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public void PutEmployee(int id,[FromBody] Empl emp)
+        {
+            Empl found=emps.Find(e => e.Empid == id);
+            emps.Remove(found);
+            emps.Insert(id, emp);
+        }
+
+        public void Delete(int id)
+        {
+            Empl f=emps.Find(e => e.Empid == id);
+            emps.Remove(f);
+        }
+
+    }
+}
---
+++ EmployeeGenralController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MVCAppWithAPI.Models;
+using Newtonsoft.Json;
+
+namespace MVCAppWithAPI.Controllers
+{
+    public class EmployeeGenralController : Controller
+    {
+        static List<Empl> list = new List<Empl>();
+        string baseurl = "http://localhost:62455";
+        // GET: EmployeesGenral
+        public async Task<ActionResult> Index()
+        {
+         
+            using (var client=new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("/api/Emp");
+                if (res.IsSuccessStatusCode)
+                {
+                    var empres = res.Content.ReadAsStringAsync().Result;
+                    list = JsonConvert.DeserializeObject<List<Empl>>(empres);
+                }
+                
+            }
+            return View(list);
+        }
+
+        // GET: EmployeesGenral/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EmployeesGenral/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeesGenral/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesGenral/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeesGenral/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesGenral/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeesGenral/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
