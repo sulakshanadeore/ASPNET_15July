df2556bb85a73e84e8e633091a7b698c88cc9694
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-08-06 16:14:28 +0530

    Add files via upload

---
+++ CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutingTrial.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class CustomersController : Controller
+    {
+        //[Route("")]
+        //[Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+        public ActionResult About()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RoutingTrial.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutingTrial.Controllers
+{
+   
+    //[Route("def")]
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Route("")]
+        [Route("Home/Index")]
+        [Route("~/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Home/Pri")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("/Con")]
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+
+        //[Route("Home/Details/{id?}")]
+
+        public ActionResult Details(int id)
+        {
+
+            return Content("Id passed = " + id);
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
