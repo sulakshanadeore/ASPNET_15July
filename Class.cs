0a090281a8afc02f5adfdb2f56122120fb330cd4
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-08-06 12:36:37 +0530

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIDemo
+{
+    public class Category
+    {
+        public int CatId { get; set; }
+        public string CatName { get; set; }
+        public string Description { get; set; }
+    }
+
+    public interface ICategory
+    {
+        List<Category> GetCategories();
+    }
+
+    public class CategoryRepository : ICategory
+    {
+        public List<Category> GetCategories()
+        {
+            //throw new System.NotImplementedException();
+            List<Category> categories = new List<Category>();
+            categories.Add(new Category { CatId = 1, CatName = "Beverages", Description = "Tea,Coffee" });
+
+            categories.Add(new Category
+            {
+                CatId = 2,
+                CatName = "Snacks",
+                Description = "Pizza,Burger"
+            });
+            return categories;
+
+        }
+    }
+
+}
---
+++ NWController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIDemo.Controllers
+{
+    public class NWController : Controller
+    {
+        public ICategory CategoryData { get; set; }
+        //Using Dependency using Constructor
+        public NWController(ICategory category)
+        {
+            this.CategoryData = category;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = CategoryData.GetCategories();
+            return View(categories);
+        }
+    }
+}
