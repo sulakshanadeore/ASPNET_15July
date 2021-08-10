76041d7155af52617a5bff1e71d4dec34f881877
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-08-10 10:59:17 +0530

    Add files via upload

---
+++ EmpExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo
+{
+    class Emp
+    {
+        public int Empid { get; set; }
+        public string Name { get; set; }
+        public int Sal { get; set; }
+    }
+    class EmpExample
+    {
+        static void Main(string[] args)
+        {
+            List<Emp> emps = new List<Emp>();
+            emps.Add(new Emp {Empid=1,Name="Ajit",Sal=20000 });
+            emps.Add(new Emp { Empid = 2, Name = "Sujit", Sal = 30000 });
+            emps.Add(new Emp { Empid = 3, Name = "Manjit", Sal = 23000 });
+            emps.Add(new Emp { Empid = 4, Name = "Jay", Sal = 25000 });
+            emps.Add(new Emp { Empid = 5, Name = "Vijay", Sal = 21000 });
+
+            var empdata = emps.Where(s => s.Sal > 21000).Select(n => n.Name).ToList();
+
+            //ToList()----- immediate
+            //No ToList()--- deferred
+            emps.Add(new Emp { Empid = 6, Name = "Harish", Sal = 27000 });
+
+
+            foreach (var item in empdata)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo
+{
+   public class Student
+    {
+        public string City { get; set; }
+        public string Name { get; set; }
+        public int[] Scores { get; set; }
+    }
+
+
+    class ObjectsDemo
+    {
+        static void Main(string[] args)
+        {
+            //List<Student> students = new List<Student>();
+            //students.Add(new Student {Name="anita",City="Pune",Scores=new int[] {78,99,66 } });
+
+            //students.Add(new Student { Name = "Sunita", City = "Mumbai",Scores = new int[] { 86, 89, 82 } });
+
+
+            //students.Add(new Student { Name = "Vinita",City="Mumbai", Scores = new int[] { 67, 82, 61 } });
+
+            //var studData = from s in students
+            //               orderby s.Scores[0]
+            //               where s.Scores[0] > 75
+            //               select s;
+
+            //foreach (var item in studData)
+            //{
+            //    Console.WriteLine(item.Name);
+            //    Console.WriteLine(item.Scores[0]);
+            //}
+
+
+            List<Student> students = new List<Student>();
+            students.Add(new Student { Name = "anita", City = "Pune", Scores = new int[] { 78, 99, 66 } });
+
+            students.Add(new Student { Name = "Sunita", City = "Mumbai", Scores = new int[] { 86, 89, 82 } });
+
+
+            students.Add(new Student { Name = "Vinita", City = "Bhopal", Scores = new int[] { 67, 82, 61 } });
+
+            students.Add(new Student { Name = "Ankita", City = "Mumbai", Scores = new int[] { 67, 82, 61 } });
+
+            students.Add(new Student { Name = "Pranita", City = "Bangalore", Scores = new int[] { 67, 82, 61 } });
+
+            students.Add(new Student { Name = "Savita", City = "Mumbai", Scores = new int[] { 67, 82, 61 } });
+
+            foreach (var item in students)
+            {
+                Console.WriteLine(item.Name + " " + item.City);
+            }
+
+            Console.WriteLine("---------------------");
+
+            //var cityGroups = students.GroupBy(s => s.City);
+            var cityGroups = students.ToLookup(s => s.City);
+
+
+            //var cityGroups = from s in students
+            //                 group s by s.City;
+                             
+
+            foreach (var item in cityGroups)
+            {
+                Console.WriteLine("Students in "  + item.Key + ": ");
+                Console.WriteLine("-------------------------");
+                foreach (var item1 in item)
+                {
+                    Console.WriteLine("* "+ item1.Name);
+
+                }
+                Console.WriteLine();
+                
+            }
+
+
+            Console.Read();
+        }
+    }
+}
